d8680b90b45f997bd2b5bd8a0198443cb0e9b955
sheldon05 <jesussantoscapote@gmail.com>
2022-11-01 09:30:48 -0400

    Create players.py

---
+++ players.py
@@ -0,0 +1,46 @@
+class Player:
+    experience_threshold = () #Rellenar en algun punto
+
+    armors = []
+
+    skills = []
+
+    __level = 1
+
+    epsilon = 1e-2
+
+    @property
+    def life(self):
+        raise NotImplementedError()
+
+    @property
+    def damage(self):
+        raise NotImplementedError()
+
+    @property
+    def level(self):
+        return self.__level
+
+    @property
+    def attacks(self):
+        raise NotImplementedError()
+
+    @property
+    def experience(self):
+        raise NotImplementedError()
+
+    def level_up(self):
+        self.__level += 1
+
+
+class Warrior(Player):
+    pass
+
+class Rogue(Player):
+    pass
+
+class Wizard(Player):
+    pass
+        
+
+    
